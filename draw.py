53341c2b76e8c892910465811ca0712ff87620f3
Mikhail Snitovets <mksn13@gmail.com>
2017-10-04 19:36:53 +0300

    Complete lab

--- draw.py
+++ draw.py
@@ -5,9 +5,10 @@ def plot_values(signal_values):
     return list(zip(*enumerate(signal_values)))
 
 
-def draw_signal_comparison(harmonic_signals_values, polyharmonic_signals_values, labels):
+def draw_signal_comparison(harmonic_signals_values, polyharmonic_signals_values, spectrum, poly_spectrum, labels):
     assert len(harmonic_signals_values) == len(labels)
     plt.subplots_adjust(bottom=0.25)
+    plt.figure(1)
     plt.subplot(1, 2, 1)
     lines = []
     for signal_values in harmonic_signals_values:
@@ -19,4 +20,18 @@ def draw_signal_comparison(harmonic_signals_values, polyharmonic_signals_values,
         x_points, y_points = plot_values(signal_values)
         line, = plt.plot(x_points, y_points)
     plt.figlegend(lines, labels, loc='lower center')
+
+    plt.figure(2)
+    axes = plt.subplot(1, 2, 1)
+    amps, phases = zip(*spectrum)
+    axes.stem(*plot_values(amps), linefmt='b', markerfmt=' ', basefmt=' ')
+    axes = plt.subplot(1, 2, 2)
+    axes.stem(*plot_values(phases), linefmt='b', markerfmt=' ', basefmt=' ')
+
+    plt.figure(3)
+    axes = plt.subplot(1, 2, 1)
+    amps, phases = zip(*poly_spectrum)
+    axes.stem(*plot_values(amps), linefmt='b', markerfmt=' ', basefmt=' ')
+    axes = plt.subplot(1, 2, 2)
+    axes.stem(*plot_values(phases), linefmt='b', markerfmt=' ', basefmt=' ')
     plt.show()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from draw import *
 from helper import randoms_from
 from signal import fourier_spectrum, repaired_signal, polyharmonic_signal
 
-N = 256
+N = 64
 TEST_SIGNAL = lambda signal_index, N: 10 * cos((2 * pi * signal_index) / N)
 TEST_AMPLITUDES = [1, 3, 5, 8, 10, 12, 16]
 TEST_PHASES = [pi / 6, pi / 4, pi / 3, pi / 2, 3 * pi / 4, pi]
@@ -22,10 +22,9 @@ if __name__ == '__main__':
     test_polyharmonic_signal = lambda signal_index, N: polyharmonic_signal(signal_index, N, 30, TEST_SPECTRUM)
     repaired_polyharmonic_signal = lambda signal_index, spectrum: spectrum[0][0] / 2 + repaired_signal(signal_index,
                                                                                                        spectrum)
-
     original_poly_signal_values = [test_polyharmonic_signal(i, N) for i in range(N)]
-    spectrum = fourier_spectrum(original_poly_signal_values)
-    repaired_poly_signal_values = [repaired_polyharmonic_signal(i, spectrum) for i in range(N)]
+    poly_spectrum = fourier_spectrum(original_poly_signal_values)
+    repaired_poly_signal_values = [repaired_polyharmonic_signal(i, poly_spectrum) for i in range(N)]
     polyharmonic_signals_values = [original_poly_signal_values, repaired_poly_signal_values]
 
-    draw_signal_comparison(harmonic_signals_values, polyharmonic_signals_values, LABELS)
+    draw_signal_comparison(harmonic_signals_values, polyharmonic_signals_values, spectrum, poly_spectrum, LABELS)
